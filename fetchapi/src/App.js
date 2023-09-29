5c1cf60cd47900ca214878e2f1e415b258681474
unknown <mprateekvernekar189@gmail.com>
2023-09-29 23:32:32 +0530

    Completed project

---
+++ fetchapi/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import {useState} from 'react';
+import Header from './Header';
+
+function App() {
+  const [students,setStudents]=useState([]);
+
+  const handleCallback=(childData)=>{
+    console.log(childData.data)
+    setStudents(childData.data);
+  }
+  
+
+  return (
+    <div className="App">
+      <Header parentCallback={handleCallback}/>
+      <div className='display'>
+        {
+          students.map((data,index)=>{
+            return(
+              <div className='card' key={index}>
+                <p>{data.first_name} {data.last_name}</p>
+                <p>{data.email}</p>
+                <img src={data.avatar} width={'200px'} height={'200px'} alt='profile'></img>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ fetchapi/src/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Header = (props) => {
+  
+    const bringStud=()=>{
+        fetch('https://reqres.in/api/users?page=1')
+        .then((res)=>{
+                return res.json()
+        })
+        .then((json)=>{
+            console.log(json);
+            props.parentCallback(json);
+        })
+    }
+
+    // const bringStud=()=>{
+    //     fetch('https://reqres.in/api/users?page=1')
+    //     .then((res) => res.json())
+    //     .then((json) => {
+    //         console.log(json);
+    //     })
+    // }
+
+    return (
+        <div className='Header'>
+        <button onClick={bringStud}>Fetch Users</button>
+        </div>
+    )
+}
+
+export default Header
